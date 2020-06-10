fcb695e10fbb7ab63226997a86517f12408bfd1d
Axel Ballarin <axelbalmir@gmail.com>
2020-06-10 11:21:46 +0200

    add choices to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,30 @@ Category.all.each do |category|
   category.destroy
 end
 
-puts "Create categories"
+Choice.all.each do |choice|
+  choice.destroy
+end
 
-fun = Category.create!({name: "Fun"})
+puts "Create categories..."
 
-humour_noir = Category.create!({name: "Humour noir"})
+debutant = Category.create!({name: "Débutant"})
+
+avance = Category.create!({name: "Avancé"})
+
+philosophique = Category.create!({name: "Philosophique"})
+
+puts "Create choices..."
+
+Choice.create!({first_choice: "Lécher la barre dans le métro", second_choice: "Mâcher un chewing-gum trouvé par terre", category: debutant})
+
+Choice.create!({first_choice: "Porter les sous-vêtements de quelqu’un d’autre", second_choice: "Utiliser la brosse à dent de quelqu’un d’autre", category: debutant})
+
+Choice.create!({first_choice: "Que tes parents te surprennent en train de faire l’amour", second_choice: "Surprendre tes parents faire l’amour", category: avance})
+
+Choice.create!({first_choice: "Qu’il ne fasse jamais nuit", second_choice: "Qu’il ne fasse jamais jour", category: philosophique})
+
+Choice.create!({first_choice: "Être ré-incarné en moustique", second_choice: "Ne pas être ré-incarné", category: philosophique})
+
+Choice.create!({first_choice: "Pouvoir changer le passé", second_choice: "Pouvoir changer le futur", category: philosophique})
+
+puts "Finished!"
