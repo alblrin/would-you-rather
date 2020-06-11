a3ce4099c6a23f4b78dfbca6659c3f76dc86083f
Axel Ballarin <axelbalmir@gmail.com>
2020-06-11 09:55:39 +0200

    add new choices index and the possibility to create your own choices

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,2 +1,33 @@
 class ChoicesController < ApplicationController
+  def index
+    @choices = Choice.all
+  end
+
+  def new
+    @choice = Choice.new
+    @categories = Category.all
+  end
+
+  def create
+    @choice = Choice.new(choice_params)
+    @category_id = params["choice"]["category"].to_i
+    @category = Category.where("id = ?", @category_id).first
+    @choice.category = @category
+    @choice.save
+
+    redirect_to choices_path
+  end
+
+  def destroy
+    @choice = Choice.find(params[:id])
+    @choice.destroy
+
+    redirect_to choices_path
+  end
+
+  private
+
+  def choice_params
+    params.require(:choice).permit(:first_choice, :second_choice, :category_id)
+  end
 end
