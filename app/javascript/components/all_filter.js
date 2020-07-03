776e1059e2fc640c09a5eaf031748f94fae745d2
Axel Ballarin <axelbalmir@gmail.com>
2020-07-03 09:34:48 +0200

    add % in when a question is answered

--- app/javascript/components/all_filter.js
+++ app/javascript/components/all_filter.js
@@ -1,8 +1,11 @@
 const allFilter = () => {
   const allChoices = document.querySelectorAll('.choice-card');
-    document.getElementById('all-filter').addEventListener("click", (event) => {
+  const filter = document.getElementById('all-filter');
+  if (filter) {
+    filter.addEventListener("click", (event) => {
     allChoices.forEach(choice => choice.classList.remove('d-none'));
-  });
+    });
+  }
 }
 
 export { allFilter };
